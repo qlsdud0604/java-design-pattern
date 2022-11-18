591d926d0bf8a8cc3c93f5769f5727f7b11e81b2
임빈영 <qlsdud960604@gmail.com>
2022-11-18 17:41:22 +0900

    singleton pattern 테스트 코드 수정

--- src/test/java/singleton_pattern/SingletonPatternTest.java
+++ src/test/java/singleton_pattern/SingletonPatternTest.java
@@ -1,15 +1,27 @@
 package singleton_pattern;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SingletonPatternTest {
 
+    private static SingleObject singleObject;
+
+    @BeforeEach
+    public void setup() {
+        singleObject = SingleObject.getInstance();
+    }
+
     @Test
     public void 싱글톤_패턴_테스트() {
+        int expectedHashCode = singleObject.hashCode();
+
         for (int i = 0; i < 5; i++) {
-            SingleObject singleObject = SingleObject.getInstance();
-            System.out.println(singleObject.hashCode());
+            SingleObject tempSingleObject = SingleObject.getInstance();
+            int actualHashCode = tempSingleObject.hashCode();
+
+            Assertions.assertEquals(expectedHashCode, actualHashCode);
         }
     }
 }
