fc20df62893b431bb9e4390f134c29ae436f2de2
임빈영 <qlsdud960604@gmail.com>
2022-11-22 15:00:48 +0900

    Rename - FactoryMethodPatternTest 클래스
    기존의 FactoryMethodPattern 클래스명을 FactoryMethodPatternTest 클래스명으로 변경

--- src/test/java/factory_method_pattern/FactoryMethodPatternTest.java
+++ src/test/java/factory_method_pattern/FactoryMethodPatternTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class FactoryMethodPattern {
+public class FactoryMethodPatternTest {
 
     private ClassA classA;
 
