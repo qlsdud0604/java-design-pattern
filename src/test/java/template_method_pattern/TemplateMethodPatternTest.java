47e200e8a15a80bf8447ef58e925233b174eb082
임빈영 <qlsdud960604@gmail.com>
2022-11-22 14:59:56 +0900

    Rename - TemplateMethodPatternTest 클래스
    기존의 TemplateMethodPattern 클래스명을 TemplateMethodPatternTest  클래스명으로 변경

--- src/test/java/template_method_pattern/TemplateMethodPatternTest.java
+++ src/test/java/template_method_pattern/TemplateMethodPatternTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class TemplateMethodPattern {
+public class TemplateMethodPatternTest {
 
     private Parent childA;
     private Parent childB;
