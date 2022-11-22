e366492de9dff860ee90c04c6743c909b9cc9f44
임빈영 <qlsdud960604@gmail.com>
2022-11-22 10:13:25 +0900

    template method pattern 테스트

---
+++ src/test/java/template_method_pattern/TemplateMethodPattern.java
@@ -0,0 +1,40 @@
+package template_method_pattern;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TemplateMethodPattern {
+
+    private Parent childA;
+    private Parent childB;
+    private Parent childC;
+
+
+    @BeforeEach
+    public void setup() {
+        childA = new ChildA();
+        childB = new ChildB();
+        childC = new ChildC();
+    }
+
+    @AfterEach
+    public void followup() {
+        System.out.println("==========");
+    }
+
+    @Test
+    public void childA_테스트() {
+        childA.someMethod();
+    }
+
+    @Test
+    public void childB_테스트() {
+        childB.someMethod();
+    }
+
+    @Test
+    public void childC_테스트() {
+        childC.someMethod();
+    }
+}
